8c95a024dd8c9233a4c024ba4df78939f2ddac63
Luis Adrian Arellano Ruiz <luis.arellano@alignet.com>
2021-04-28 19:38:01 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2019-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Predicción del Mercado de Valores")
+
+stocks = ("AAPL", "GOOG", "MSFT", "GME")
+selected_stocks = st.selectbox("Seleccione el dataset para la predicción", stocks)
+
+n_years = st.slider("Años de predicción:", 1, 5)
+period = n_years * 365
+
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace = True)
+    return data
+
+data_load_state = st.text("Cargando información...")
+data = load_data(selected_stocks)
+data_load_state.text("Listo!")
+
+st.subheader('DataSet')
+st.write(data.tail())
+
+def plot_raw_data ():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Serie de Datos Cronológico", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date":"ds", "Close":"y"})
+
+m = Prophet()
+
+m.fit(df_train)
+
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader("Información de la predicción")
+st.write(forecast.tail())
+
+st.write("Gráfico de la predicción")
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Información de los componentes")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
